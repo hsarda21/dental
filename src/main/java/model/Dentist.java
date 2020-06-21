384f61a5545b9ff9ed593ffda863ca86e51d32ef
Time <hsarda21@gmail.com>
2020-06-21 19:49:52 +0530

    Added basic entities. closes #1

---
+++ src/main/java/model/Dentist.java
@@ -0,0 +1,18 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Dentist extends Person
+{
+    private Integer experience;
+}
---
+++ src/main/java/model/Patient.java
@@ -0,0 +1,18 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Patient extends Person
+{
+
+}
