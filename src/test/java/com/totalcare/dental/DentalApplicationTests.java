da7d216a4bdbba53de9083bdf283915b89b754b2
Time <hsarda21@gmail.com>
2020-06-21 19:34:24 +0530

    Initial commit

---
+++ src/test/java/com/totalcare/dental/DentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.totalcare.dental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
